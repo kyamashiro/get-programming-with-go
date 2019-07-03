cdba96b9197863a5f2cfdd3d827204d9f8f7c9bf
kyamashiro <yamashiro@rrr-z.co.jp>
2019-07-04 08:23:40 +0900

    エラーの変数化

--- error/sudoku1/sudoku1.go
+++ error/sudoku1/sudoku1.go
@@ -1,4 +1,4 @@
-package sudoku1
+package main
 
 import (
 	"errors"
@@ -10,9 +10,21 @@ const rows, columns = 9, 9
 
 type Grid [rows][columns]int8
 
+/*
+エラーメッセージはユーザインタフェースの一部
+エラーメッセージを代入する変数の名前は、Errではじめる
+*/
+var (
+	// 境界外
+	ErrBounds = errors.New("out of bounds")
+	// 不正な数
+	ErrDigit = errors.New("invalid digit")
+)
+
 func (g *Grid) Set(row, column int, digit int8) error {
+	// 例外的な値をエラー処理する
 	if !inBounds(row, column) {
-		return errors.New("out of bounds")
+		return ErrBounds
 	}
 
 	g[row][column] = digit
@@ -33,8 +45,14 @@ func inBounds(row, column int) bool {
 func main() {
 	var g Grid
 	err := g.Set(10, 0, 5)
+	// error.Newはポインタを使って実装されているので、このswitch文はエラーメッセージのテキストではなくメモリアドレスを比較している
 	if err != nil {
-		fmt.Printf("An error occured %+v\n", err)
+		switch err {
+		case ErrBounds, ErrDigit:
+			fmt.Println("Error")
+		default:
+			fmt.Println(err)
+		}
 		os.Exit(1)
 	}
 }
